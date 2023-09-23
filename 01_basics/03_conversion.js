ce9bef33300b09452cb7321f1d9a0f2c7916cbce
Mohammad Zaid <89009941+mohammadzaid07@users.noreply.github.com>
2023-09-23 12:45:39 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let score = "33aa"
+
+console.log(typeof score);
+
+let value = Number(score)
+
+console.log(typeof value)
+console.log(value);
+
+// "33" -> 33 
+// "33abc" -> NaN 
+// null -> 0 
+// undefined -> NaN 
+// true - 1 , false-> 0 
